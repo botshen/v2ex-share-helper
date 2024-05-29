3e0067b3127f0767aa3629734c04877a93a758d0
Xin shen <363088847@qq.com>
2024-05-29 23:23:44 +0800

    文案

--- src/tabs/App.tsx
+++ src/tabs/App.tsx
@@ -238,7 +238,7 @@ export default function DeltaFlyerPage() {
               <div className="w-full mt-4">
                 <footer className="bg-[#f9f9f9] rounded-md   p-2"> 
                     <div className="text-gray-700 text-lg text-center">
-                      在Chrome商店搜索"v2ex share"即可安装
+                      Chrome扩展商店搜 <span className='text-orange-700'>v2ex share </span>一键分享
                     </div>
                  </footer>
               </div>
