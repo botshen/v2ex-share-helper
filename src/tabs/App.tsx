e715769de972d600067982f6b1c193dcbc2a15e9
Xin shen <363088847@qq.com>
2025-01-17 22:48:57 +0800

    lint

--- src/tabs/App.tsx
+++ src/tabs/App.tsx
@@ -1,214 +1,235 @@
-import { ReloadIcon } from "@radix-ui/react-icons";
-import download from 'downloadjs';
-import { toPng } from 'html-to-image';
-import { Resizable } from "re-resizable";
-import { useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
-import { CommentsSection } from '~components/CommentsSection';
-import { Footer } from '~components/Footer';
-import { Header } from '~components/Header';
-import { PostContent } from '~components/PostContent';
-import { QrCode } from '~components/QrCode';
-import { SubPost } from '~components/SubPost';
-import { Button } from "~components/ui/button";
-import { toastStyles } from '~const';
-import '../style.css';
-const notify = () => toast('已复制到剪贴板📋', { icon: '✅' });
-const notifyError = () => toast('复制图片失败', { icon: '❌' });
-const notifyCopyUrl = () => toast('URL已复制到剪贴板📋', { icon: '✅' });
-import './app.css';
+import { ReloadIcon } from "@radix-ui/react-icons"
+import download from "downloadjs"
+import { toPng } from "html-to-image"
+import { Resizable } from "re-resizable"
+import { useState } from "react"
+import toast, { Toaster } from "react-hot-toast"
+
+import { CommentsSection } from "~components/CommentsSection"
+import { Footer } from "~components/Footer"
+import { Header } from "~components/Header"
+import { PostContent } from "~components/PostContent"
+import { QrCode } from "~components/QrCode"
+import { SubPost } from "~components/SubPost"
+import { Button } from "~components/ui/button"
+import { toastStyles } from "~const"
+
+import "../style.css"
+import "./app.css"
+
+const notify = () => toast("已复制到剪贴板📋", { icon: "✅" })
+const notifyError = () => toast("复制图片失败", { icon: "❌" })
+const notifyCopyUrl = () => toast("URL已复制到剪贴板📋", { icon: "✅" })
+
 export default function DeltaFlyerPage() {
-  const [postContent, setPostContent] = useState<string>("");
-  const [title, setTitle] = useState<string>("");
-  const [author, setAuthor] = useState<string>("");
-  const [avatarUrl, setAvatarUrl] = useState<string>("");
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [postscripts, setPostscripts] = useState<Postscript[]>([]);
-  const [showQrCode, setShowQrCode] = useState<boolean>(true);
-  const [showSubPost, setShowSubPost] = useState<boolean>(true);
-  const [showComments, setShowComments] = useState<boolean>(true);
-  const [showPost, setShowPost] = useState<boolean>(true);
-  const [showAllComments, setShowAllComments] = useState<boolean>(false);
-  const [url, setUrl] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [loadingDownload, setLoadingloadingDownload] = useState<boolean>(false);
-  const [previewMode, setPreviewMode] = useState<boolean>(false);
-  const [selectedComments, setSelectedComments] = useState<Set<number>>(new Set());
-  const [themeColor, setThemeColor] = useState<string>('white'); // 添加主题颜色状态
+  const [postContent, setPostContent] = useState<string>("")
+  const [title, setTitle] = useState<string>("")
+  const [author, setAuthor] = useState<string>("")
+  const [avatarUrl, setAvatarUrl] = useState<string>("")
+  const [comments, setComments] = useState<Comment[]>([])
+  const [postscripts, setPostscripts] = useState<Postscript[]>([])
+  const [showQrCode, setShowQrCode] = useState<boolean>(true)
+  const [showSubPost, setShowSubPost] = useState<boolean>(true)
+  const [showComments, setShowComments] = useState<boolean>(true)
+  const [showPost, setShowPost] = useState<boolean>(true)
+  const [showAllComments, setShowAllComments] = useState<boolean>(false)
+  const [url, setUrl] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
+  const [loadingDownload, setLoadingloadingDownload] = useState<boolean>(false)
+  const [previewMode, setPreviewMode] = useState<boolean>(false)
+  const [selectedComments, setSelectedComments] = useState<Set<number>>(
+    new Set()
+  )
+  const [themeColor, setThemeColor] = useState<string>("white") // 添加主题颜色状态
 
   const handleThemeChange = (color: string) => {
-    setThemeColor(color);
-  };
+    setThemeColor(color)
+  }
 
   const handleMessage = (message: any) => {
     if (message.action === "showPostContent") {
-      const { postContent, title, author, avatarUrl, comments, postscripts, url } = message.data;
-      setPostContent(postContent);
-      setTitle(title);
-      setAuthor(author);
-      setAvatarUrl(avatarUrl);
-      setComments(comments);
-      setPostscripts(postscripts);
+      const {
+        postContent,
+        title,
+        author,
+        avatarUrl,
+        comments,
+        postscripts,
+        url
+      } = message.data
+      setPostContent(postContent)
+      setTitle(title)
+      setAuthor(author)
+      setAvatarUrl(avatarUrl)
+      setComments(comments)
+      setPostscripts(postscripts)
       setUrl(url)
     }
-  };
+  }
 
-  chrome.runtime.onMessage.addListener(handleMessage);
+  chrome.runtime.onMessage.addListener(handleMessage)
 
   const handleCommentChange = (index: number) => {
-    const updatedSet = new Set(selectedComments);
+    const updatedSet = new Set(selectedComments)
     if (updatedSet.has(index)) {
-      updatedSet.delete(index);
+      updatedSet.delete(index)
     } else {
-      updatedSet.add(index);
+      updatedSet.add(index)
     }
-    setSelectedComments(updatedSet);
-  };
+    setSelectedComments(updatedSet)
+  }
 
   const togglePreviewMode = () => {
-    setPreviewMode(!previewMode);
-  };
+    setPreviewMode(!previewMode)
+  }
 
   const convertImageToBase64 = async (imgUrl: string): Promise<string> => {
     try {
       // 使用 chrome.runtime.getURL 来避免跨域问题
-      const response = await fetch(imgUrl);
-      const blob = await response.blob();
+      const response = await fetch(imgUrl)
+      const blob = await response.blob()
       return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result as string);
-        reader.onerror = reject;
-        reader.readAsDataURL(blob);
-      });
+        const reader = new FileReader()
+        reader.onloadend = () => resolve(reader.result as string)
+        reader.onerror = reject
+        reader.readAsDataURL(blob)
+      })
     } catch (error) {
-      console.error('转换图片失败:', error);
-      return '';
+      console.error("转换图片失败:", error)
+      return ""
     }
-  };
+  }
 
   const copyImageToClipboard = async () => {
-    const element = document.getElementById("post-content");
+    const element = document.getElementById("post-content")
     if (element) {
       try {
-        setLoading(true);
-        
-        const { width, height } = element.getBoundingClientRect();
-        
+        setLoading(true)
+
+        const { width, height } = element.getBoundingClientRect()
+
         const dataUrl = await toPng(element, {
           fetchRequestInit: {
-            cache: 'no-cache',
+            cache: "no-cache"
           },
           pixelRatio: 3,
           quality: 1,
           width: width,
           height: height,
           style: {
-            transform: 'scale(1)',
-            transformOrigin: 'top left'
+            transform: "scale(1)",
+            transformOrigin: "top left"
           },
           filter: (node) => {
             if (node instanceof HTMLElement) {
-              node.style.textRendering = 'optimizeLegibility';
-              node.style.webkitFontSmoothing = 'antialiased';
+              node.style.textRendering = "optimizeLegibility"
+              node.style.webkitFontSmoothing = "antialiased"
             }
-            return true;
+            return true
           }
-        });
-        
-        const blob = await (await fetch(dataUrl)).blob();
-        const item = new ClipboardItem({ "image/png": blob });
-        await navigator.clipboard.write([item]);
-        setLoading(false);
-        notify();
+        })
+
+        const blob = await (await fetch(dataUrl)).blob()
+        const item = new ClipboardItem({ "image/png": blob })
+        await navigator.clipboard.write([item])
+        setLoading(false)
+        notify()
       } catch (error) {
-        console.error('复制图片失败:', error);
-        setLoading(false);
-        notifyError();
+        console.error("复制图片失败:", error)
+        setLoading(false)
+        notifyError()
       }
     }
-  };
+  }
 
   const downloadImage = async () => {
-    const element = document.getElementById("post-content");
+    const element = document.getElementById("post-content")
     if (element) {
       try {
-        setLoadingloadingDownload(true);
-        
+        setLoadingloadingDownload(true)
+
         // 预处理所有第三方图片
-        const images = element.getElementsByTagName('img');
+        const images = element.getElementsByTagName("img")
         for (const img of images) {
-          if (img.getAttribute('rel') === 'noreferrer') {
-            const base64Url = await convertImageToBase64(img.src);
+          if (img.getAttribute("rel") === "noreferrer") {
+            const base64Url = await convertImageToBase64(img.src)
             if (base64Url) {
-              img.src = base64Url;
-              img.removeAttribute('rel');
+              img.src = base64Url
+              img.removeAttribute("rel")
             }
           }
         }
 
         const dataUrl = await toPng(element, {
           fetchRequestInit: {
-            cache: 'no-cache',
+            cache: "no-cache"
           },
           pixelRatio: 2,
           quality: 1,
           style: {
-            transform: 'scale(1)',
-            transformOrigin: 'top left'
+            transform: "scale(1)",
+            transformOrigin: "top left"
           }
-        });
-        
-        setLoadingloadingDownload(false);
-        download(dataUrl, 'v2ex.png');
+        })
+
+        setLoadingloadingDownload(false)
+        download(dataUrl, "v2ex.png")
       } catch (error) {
-        console.error('下载图片失败:', error);
-        setLoadingloadingDownload(false);
+        console.error("下载图片失败:", error)
+        setLoadingloadingDownload(false)
       }
     }
-  };
+  }
 
   const handleShowAllComments = (checked: boolean) => {
-    setShowAllComments(checked);
+    setShowAllComments(checked)
     if (checked) {
-      setSelectedComments(new Set(comments.map((_, index) => index)));
+      setSelectedComments(new Set(comments.map((_, index) => index)))
     } else {
-      setSelectedComments(new Set());
-    } 
-  };
+      setSelectedComments(new Set())
+    }
+  }
 
   const copyUrlToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText(url);
-      notifyCopyUrl();
+      await navigator.clipboard.writeText(url)
+      notifyCopyUrl()
     } catch (error) {
-      console.error('复制URL失败:', error);
+      console.error("复制URL失败:", error)
     }
-  };
+  }
 
   return (
     <div className={`flex flex-col items-center p-6 min-h-screen bg-slate-300`}>
       <div className="fixed top-5 right-5 flex flex-col justify-center p-4 gap-4 z-50 bg-white border-black rounded-lg shadow-lg">
         <Button
-          className={loading || !previewMode ? "bg-gray-400 text-gray-700 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-700 text-white font-bold"}
+          className={
+            loading || !previewMode
+              ? "bg-gray-400 text-gray-700 cursor-not-allowed"
+              : "bg-blue-500 hover:bg-blue-700 text-white font-bold"
+          }
           disabled={loading || !previewMode}
-          onClick={copyImageToClipboard}
-        >
+          onClick={copyImageToClipboard}>
           {loading && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
           复制为图片
         </Button>
         <Button
-          className={loadingDownload || !previewMode ? "bg-gray-400 text-gray-700 cursor-not-allowed" : "bg-green-500 hover:bg-green-700 text-white font-bold"}
+          className={
+            loadingDownload || !previewMode
+              ? "bg-gray-400 text-gray-700 cursor-not-allowed"
+              : "bg-green-500 hover:bg-green-700 text-white font-bold"
+          }
           onClick={downloadImage}
-          disabled={loadingDownload || !previewMode}
-        >
-          {loadingDownload && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
+          disabled={loadingDownload || !previewMode}>
+          {loadingDownload && (
+            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          )}
           保存为图片
         </Button>
         <Button
           className="bg-red-500 hover:bg-red-700 text-white font-bold"
           variant="destructive"
-          onClick={togglePreviewMode}
-        >
+          onClick={togglePreviewMode}>
           {previewMode ? "退出预览" : "预览图片"}
         </Button>
         {postContent.length > 0 && (
@@ -275,8 +296,7 @@ export default function DeltaFlyerPage() {
               />
               <Button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold"
-                onClick={copyUrlToClipboard}
-              >
+                onClick={copyUrlToClipboard}>
                 复制URL
               </Button>
             </div>
@@ -289,24 +309,19 @@ export default function DeltaFlyerPage() {
         <div className="grid grid-cols-3 gap-2">
           <button
             className={`w-8 h-8 rounded-full border-2 bg-white border-gray-950 `}
-            onClick={() => handleThemeChange('white')}
-          ></button>
+            onClick={() => handleThemeChange("white")}></button>
           <button
             className={`w-8 h-8 rounded-full border-2 bg-green-100 border-gray-950 `}
-            onClick={() => handleThemeChange('green')}
-          ></button>
+            onClick={() => handleThemeChange("green")}></button>
           <button
             className={`w-8 h-8 rounded-full border-2 bg-yellow-100 border-gray-950 `}
-            onClick={() => handleThemeChange('yellow')}
-          ></button>
+            onClick={() => handleThemeChange("yellow")}></button>
           <button
             className={`w-8 h-8 rounded-full border-2 bg-gray-500 border-gray-950 `}
-            onClick={() => handleThemeChange('gray')}
-          ></button>
+            onClick={() => handleThemeChange("gray")}></button>
           <button
             className={`w-8 h-8 rounded-full border-2 bg-black border-gray-950 `}
-            onClick={() => handleThemeChange('black')}
-          ></button>
+            onClick={() => handleThemeChange("black")}></button>
         </div>
       </div>
 
@@ -316,14 +331,17 @@ export default function DeltaFlyerPage() {
         minWidth={390}
         maxWidth={1000}
         handleComponent={{
-          right: <div className="absolute top-0 right-0 h-full w-2 bg-[#101729] cursor-ew-resize"></div>,
+          right: (
+            <div className="absolute top-0 right-0 h-full w-2 bg-[#101729] cursor-ew-resize"></div>
+          )
         }}
-        enable={{ right: previewMode ? false : true }}
-      >
+        enable={{ right: previewMode ? false : true }}>
         <div id="post-content" className={`p-6 shadow-md theme-${themeColor}`}>
           <Header title={title} avatarUrl={avatarUrl} author={author} />
           {showPost && <PostContent postContent={postContent} />}
-          {showSubPost && postscripts.length > 0 && <SubPost postscripts={postscripts} />}
+          {showSubPost && postscripts.length > 0 && (
+            <SubPost postscripts={postscripts} />
+          )}
           {showComments && comments.length > 0 && (
             <CommentsSection
               comments={comments}
@@ -338,5 +356,5 @@ export default function DeltaFlyerPage() {
       </Resizable>
       <Toaster toastOptions={{ style: toastStyles }} />
     </div>
-  );
+  )
 }
